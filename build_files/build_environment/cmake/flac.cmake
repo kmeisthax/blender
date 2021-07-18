8b71451caeece141e0d6eaa68207aaa4cc988d3f
David Wendt <dcrkid@yahoo.com>
2021-07-18 12:29:33 -0600

    Tell `libflac` to pull our copy of OGG instead of looking for it in the system

--- build_files/build_environment/cmake/flac.cmake
+++ build_files/build_environment/cmake/flac.cmake
@@ -16,12 +16,19 @@
 #
 # ***** END GPL LICENSE BLOCK *****
 
+if("${CMAKE_SYSTEM_NAME}" STREQUAL "iOS")
+  #libflac needs some extra help to find the OGG library, don't know why.
+  set(FLAC_EXTRA_ARGS --with-ogg=${LIBDIR}/ogg)
+else()
+  set(FLAC_EXTRA_ARGS)
+endif()
+
 ExternalProject_Add(external_flac
   URL file://${PACKAGE_DIR}/${FLAC_FILE}
   DOWNLOAD_DIR ${DOWNLOAD_DIR}
   URL_HASH ${FLAC_HASH_TYPE}=${FLAC_HASH}
   PREFIX ${BUILD_DIR}/flac
-  CONFIGURE_COMMAND ${CONFIGURE_ENV} && cd ${BUILD_DIR}/flac/src/external_flac/ && ${CONFIGURE_COMMAND} --prefix=${LIBDIR}/flac --disable-shared --enable-static
+  CONFIGURE_COMMAND ${CONFIGURE_ENV} && cd ${BUILD_DIR}/flac/src/external_flac/ && ${CONFIGURE_COMMAND} --prefix=${LIBDIR}/flac --disable-shared --enable-static ${FLAC_EXTRA_ARGS}
   BUILD_COMMAND ${CONFIGURE_ENV} && cd ${BUILD_DIR}/flac/src/external_flac/ && make -j${MAKE_THREADS}
   INSTALL_COMMAND ${CONFIGURE_ENV} && cd ${BUILD_DIR}/flac/src/external_flac/ && make install
   INSTALL_DIR ${LIBDIR}/flac
